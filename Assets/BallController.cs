ee61f7565bce353540c9cbac286700fbb9aa36ed
jeel11p <jeel11patel@gmail.com>
2025-01-21 12:53:31 -0800

    Added ball jump movement

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,13 +2,39 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody; 
+    [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float jumpForce = 5f;      
 
-    // This method will be called by the InputManager to move the ball
+    private bool isGrounded = false; // Check if the ball is on the ground
+
     public void MoveBall(Vector2 input)
     {
         Vector3 inputXZPlane = new(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
+
+    // Check for jump input
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false; // Prevent further jumps until grounded
+    }
+
+    // Detect collision with the ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; // Set grounded when touching the ground
+        }
     }
 }
